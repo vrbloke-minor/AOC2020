5e51f91d51e454323ec4d9674f48cd08e986c64b
vrbloke <stanislawwis@gmail.com>
2020-12-02 22:29:52 +0100

    day 2 skeleton

--- src/day2/TobogganPassVerifier.scala
+++ src/day2/TobogganPassVerifier.scala
@@ -5,4 +5,16 @@ import java.util.Scanner
 
 class TobogganPassVerifier(filepath: String) {
   val scanner = new Scanner(new File(filepath))
+  val annotatedPasswords: List[(Int, Int, Char, String)] = scanAnnotatedPasswords()
+
+  def scanAnnotatedPasswords(): List[(Int, Int, Char, String)] = {
+    var annotatedPasswords: List[(Int, Int, Char, String)] = List()
+    while(scanner.useDelimiter("\n").hasNext) {
+      val next = scanner.next().split(" ")
+      annotatedPasswords :+= (next(0).split("-")(0), next(0).split("-")(1),
+                            next(1)(0), next(2))
+    }
+    println(annotatedPasswords.mkString("\n"))
+    annotatedPasswords
+  }
 }
--- src/day2/main2.scala
+++ src/day2/main2.scala
@@ -1,5 +1,5 @@
 package day2
 
-object main2 {
-
+object main2 extends App {
+  val puzzleSolver = new TobogganPassVerifier("input/input2.txt")
 }
